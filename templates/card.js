912a43be6fbfd3afd90f1457133424fdecf53d21
Thomas <thomas.mcphee@student.manchester.ac.uk>
2026-02-03 17:37:53 +0000

    Changed client logic to use the card objects returned by the API

--- templates/card.js
+++ templates/card.js
@@ -1,11 +1,4 @@
 
-
-// FUNCTIONS
-/* SetDivPosition()
-@param div  : DOM div element
-@param x    : x position of the elements centre
-@param y    : y position of the elements centre
-*/
 export function SetDivPosition(div, x, y) {
 	let width = div.offsetWidth;
 	let height = div.offsetHeight;
@@ -13,58 +6,27 @@ export function SetDivPosition(div, x, y) {
 	div.style.top = y-(height/2)+'px';
 }
 
-/* GetCardImageSrc()
-@param rank  : RANK
-@param suit  : SUIT
-@returns src : a url for that cards image
-*/
-export function GetCardImageSrc(rank, suit, faceUp) {
+
+export function GetCardImageSrc(card, faceUp) {
 	if (faceUp == true) {
-		return 'https://deckofcardsapi.com/static/img/' + rank + suit + '.png';
+		return card.image
 	} else {
 		return 'https://deckofcardsapi.com/static/img/back.png';
 	}
 }
 
-/* CreateCard()
-@param xIn      : int
-@param yIn      : int
-@param rankIn   : RANK(s)
-@param suitIn   : SUIT(s)
-@param faceUpIn : boolean
-
-Creates the DOM elements used for rendering the card, and adds them to the html
-Sets the DOM's position on the screen
-Returns a card object:
-	fields
-		x, y, rank, suit, faceUp, e (element)
-*/
-export function CreateCard(BOARD, xIn, yIn, rankIn, suitIn, faceUpIn) {
+export function CreateCard(BOARD, card_object, x, y, faceUp) {
 	// create the div element
 	const e = document.createElement('div');
 	e.className = 'OBJ_card'
-	e.style.position = 'absolute';
 
 	// create the image element (div > element)
 	const i = document.createElement('img');
-	i.style['object-fit'] = 'cover';
-	i.style.width = 100+'%';
-	i.style.height = 100+'%';
-	i.src = GetCardImageSrc(rankIn, suitIn, faceUpIn);
+	i.className = 'IMG_card'
+	i.src = GetCardImageSrc(card_object, faceUp);
 	e.appendChild(i);
 
 	BOARD.appendChild(e);
-	SetDivPosition(e, xIn, yIn);
-
-	const card = {
-		x: xIn,
-		y: yIn,
-		rank: rankIn,
-		suit: suitIn,
-		faceUp: faceUpIn,
-		e: e,
-	}
-
-	return card
+	SetDivPosition(e, x, y);
 }
 
--- templates/client.js
+++ templates/client.js
@@ -5,33 +5,25 @@ const BOARD = document.getElementById('DIV_gameContainer')
 const BOARD_WIDTH = BOARD.offsetWidth
 const BOARD_HEIGHT = BOARD.offsetHeight
 
-const SUITS = {
-	HEARTS : 'H',
-	SPADES : 'S',
-	DIAMONDS : 'D',
-	CLUBS : 'C',
+
+let card1 = {
+    "code": "5S", 
+    "image": "https://deckofcardsapi.com/static/img/5S.png", 
+    "value": "5", 
+    "suit": "SPADES"
 }
 
-const RANKS = {
-	A : 'A',
-	1 : '1',
-	2 : '2',
-	3 : '3',
-	4 : '4',
-	5 : '5',
-	6 : '6',
-	7 : '7',
-	8 : '8',
-	9 : '9',
-	10 : '10',
-	J : 'J',
-	Q : 'Q',
-	K : 'K',
+let card2 = {
+    "code": "QH", 
+    "image": "https://deckofcardsapi.com/static/img/QH.png", 
+    "value": "Q", 
+    "suit": "HEARTS"
 }
 
 main();
 
+
 function main() {
-	CreateCard(BOARD, BOARD_WIDTH/2, BOARD_HEIGHT/2, RANKS['5'], SUITS['HEARTS'], true)
-	CreateCard(BOARD, BOARD_WIDTH/2 + 300, 300, RANKS['A'], SUITS['SPADES'], false)
+	CreateCard(BOARD, card1, 500, 300, true)
+	CreateCard(BOARD, card2, 200, 300, false)
 }
