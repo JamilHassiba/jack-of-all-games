808c2f72cde07cc4c4e1cf8c5f11bbd99da5c885
Wai Hei CHAN <wai.chan-12@student.manchester.ac.uk>
2026-02-03 17:56:55 +0000

    Initialised card api

---
+++ api.py
@@ -0,0 +1,65 @@
+import requests # used for api
+
+
+# DOCUMENTATION 
+#
+# Developed by Ryan. 
+# If you want to make a new deck just do Deck(). It will automatically call the api and make a new deck and assign the correct id. 
+# You can customise the deck you want with the parameters, but right now I only support a single deck per deck object. 
+# If you want multiple decks please make multiple deck objects. It's way less trouble for me. 
+# 
+# TODO 
+# - Add reshuffle 
+# - Add piles 
+
+class Deck: 
+    def __init__(self, id=None, shuffle=True, decks=1, jokers=False): 
+        self.url = "https://deckofcardsapi.com/api/deck/"
+        if id: 
+            self.id = id 
+        else: 
+            self.id = self.new(shuffle, decks, jokers)
+    
+    def new(self, shuffle, decks, jokers):
+        params = {
+            "jokers_enabled": jokers, 
+            "deck_count": decks, 
+        }
+
+        if shuffle: 
+            url = self.url + "new/shuffle/" 
+            response = requests.get(url, params=params)
+        else: 
+            url = self.url + "new/"
+            response = requests.get(url, params=params)
+        
+        if response.status_code == 200: 
+            data = response.json()
+            deck_id = data["deck_id"]
+            return deck_id
+        else: 
+            return None 
+
+
+    def draw(self, count=1): 
+        params = {
+            "count":count
+        }
+        if self.id: 
+            images = []
+            url = self.url + f"{self.id}/draw/" 
+            response = requests.get(url, params=params)
+            if response.status_code == 200: 
+                data = response.json() 
+                for i in data['cards']: 
+                    images.append(i["image"])
+            
+            return images 
+        else: 
+            raise AttributeError("No attribute id for class deck. You somehow removed the id from the deck object.")
+
+myDeck = Deck() 
+card = myDeck.draw()
+print(card)
+
+
--- backend.py
+++ backend.py
@@ -1,20 +1,24 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world(): 
-    return "Hello World"
 
+# dummy code to show basic flask
+@app.route('/')                
+def home_page(): 
+    return redirect("/login")
 
-@app.route("/lmao")
-def lmao(): 
-    return "Lmao"
-
-@app.route("/login")
+# basic POST handling demo 
+@app.route("/login", methods=["GET", "POST"])
 def login(): 
     if request.method == "POST": 
-        pass 
+        name = request.form['username']
+        # validate with database here 
+
+        return f"Hello {name}, POST request received"
+        # redirect to another page here 
+    return render_template("login.html")
         
 
 
