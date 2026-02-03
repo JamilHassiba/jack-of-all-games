6532b01689a5c3dbad74d1c80d2259441dfefcf5
Thomas <thomas.mcphee@student.manchester.ac.uk>
2026-02-03 15:46:12 +0000

    Got working: Rendering a card object to the screen
    Implemented js functions
    - CreateCardObject(x,y,rank,suit)
    - SetDivPosition(div,x,y)
    
    Changes to html:
    - linked and deferred js

---
+++ templates/client.js
@@ -0,0 +1,45 @@
+
+const BOARD = document.getElementById('DIV_gameContainer')
+const BOARD_WIDTH = BOARD.offsetWidth
+const BOARD_HEIGHT = BOARD.offsetHeight
+
+/* SetDivPosition()
+@param div  : DOM div element
+@param x    : x position of the elements centre
+@param y    : y position of the elements centre
+*/
+function SetDivPosition(div, x, y) {
+	let width = div.offsetWidth;
+	let height = div.offsetHeight;
+	div.style.left = x-(width/2)+'px';
+	div.style.top = y-(width/2)+'px';
+}
+
+/* CreateCardObject()
+
+*/
+function CreateCardObject(xIn, yIn, rankIn, suitIn) {
+	const e = document.createElement('div');
+	e.className = 'OBJ_card'
+	BOARD.appendChild(e);
+
+	// style
+	e.style.position = 'absolute';
+	e.style.background = '#FEFEFE';
+	e.style.width = 100+'px';
+	e.style.height = 140+'px';
+
+	SetDivPosition(e, BOARD_WIDTH/2, BOARD_HEIGHT/2);
+
+	const card = {
+		x: xIn,
+		y: yIn,
+		rank: rankIn,
+		suit: suitIn,
+		e: e,
+	}
+
+	return card
+}
+
+CreateCardObject(0,0,"A","H")
