45fff9ff4401c805783ce5466833f5f7f2948f53
Thomas <thomas.mcphee@student.manchester.ac.uk>
2026-02-10 23:06:25 +0000

    removed utils.js, client.js, client.html, sockettest.html

--- static/card.js
+++ static/card.js
@@ -69,3 +69,50 @@ export class Card {
 		this.div.remove();
 	}
 }
+
+/*
+
+export function DragDiv(div) {
+	let x = 0;
+	let y = 0;
+	let nx = 0;
+	let ny = 0;
+
+	div.onmousedown = DragBegan;
+
+	function DragBegan(e) {
+		e = e || window.event;
+		e.preventDefault();
+
+		// get the mouse cursor position at startup
+		x = e.clientX;
+		y = e.clientY;
+
+		PutDivOnTop(div)
+
+		document.onmouseup = DragEnd;
+		document.onmousemove = Drag;
+	}
+
+	function Drag(e) {
+		e = e || window.event;
+		e.preventDefault();
+
+		// calculate the new cursor position
+		nx = x - e.clientX;
+		ny = y - e.clientY;
+
+		div.style.top = (div.offsetTop - ny) + 'px';
+		div.style.left = (div.offsetLeft - nx) + 'px';
+
+		x = e.clientX;
+		y = e.clientY;
+	}
+
+	function DragEnd() {
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
+}
+
+*/
