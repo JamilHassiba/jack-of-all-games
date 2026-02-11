785aca87bab825100bf608c549efe4933c97e976
Wai Hei CHAN <wai.chan-12@student.manchester.ac.uk>
2026-02-11 01:28:35 +0000

    new way to send data from backend to frontend

--- backend.py
+++ backend.py
@@ -1,13 +1,23 @@
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, request, render_template, redirect, session, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 from flask_socketio import SocketIO
 from user import User 
+from flask_session import Session 
+import random
 
-users = [] 
+users = {}
 app = Flask(__name__)
 app.secret_key = "jack_of_all_games_secret_key"
-socket = SocketIO(app, cors_allowed_origins="*", ping_interval=2, ping_timeout=1)
+app.config['SECRET_KEY'] = 'jack_of_all_games_secret_key'  
+app.config['SESSION_TYPE'] = 'filesystem'                  # sessions stored server side 
+app.config['SESSION_PERMANENT'] = True                     # persistent sessions even after browser closes 
+app.config['PERMANENT_SESSION_LIFETIME'] = 3600            # keep session on server for x seconds  
+Session(app)
+
+
+
+
 # need a secret key for the session retained data
 def db():
     # best way to operate on db with sqlite
@@ -98,27 +108,20 @@ def logout():
     session.clear()
     return redirect("/login")
 
-@app.route("/websocket_test")
-def websocket_test(): 
-    return render_template("sockettest.html")
+@app.route("/test_conn")
+def ryan(): 
+    return render_template("conn_test.html")
 
-@socket.on("new_user")
-def new_user(args): 
-    print("user connected")
-    session["sid"] = args["sid"]
-    temp_user = User(args["sid"])
-    users.append(temp_user)
-    print(f"added new user with sid {args['sid']}, new users array: {users}")
-
-@socket.on("disconnect")
-def user_disconnect(): 
-    global users 
-    print("user disconnected")
-    sid = session["sid"]
-    users = [user for user in users if user.sid != sid]
-    print(f"removed user with sid {sid}. new user array: {users}")
+@app.route("/send_data")
+def send_data(): 
+    username = session.get("username")
+    print(username)
+    if username: 
+        return jsonify({'username': session["username"]})
+    else: 
+        return jsonify({'username': "UNKNOWN USER"})
 
     
 
 if __name__ == '__main__': 
-    socket.run(app=app, debug=True, host="0.0.0.0")
+    app.run(host="0.0.0.0")
