a06129b795e7671cb483c0e15280fb6e1740f7b1
Thomas McPhee <thomas.mcphee@student.manchester.ac.uk>
2026-03-09 11:09:48 +0000

    added blackjack files

---
+++ static/games/blackjack.js
@@ -0,0 +1,12 @@
+import {Card} from '../card.js';
+import {Deck} from '../deck.js';
+import {SendData} from '../utils.js';
+
+console.log("blackjack.js is running...")
+
+// Create socket object;
+//var socket = io();
+
+// Set the title
+Title.innerHTML = "Blackjack | 0123";
+
