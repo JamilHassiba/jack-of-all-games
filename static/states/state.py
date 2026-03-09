4e4f902406c95462537c2d6fdf7e14efbdc31caf
Thomas McPhee <thomas.mcphee@student.manchester.ac.uk>
2026-03-09 15:09:20 +0000

    Implemented state

---
+++ static/states/state.py
@@ -0,0 +1,12 @@
+class State():
+    def __init__(self, root):
+        self.__root = root
+
+    def OnEnter(self):
+        pass
+
+    def Update(self, dt):
+        pass
+
+    def OnExit(self):
+        pass
