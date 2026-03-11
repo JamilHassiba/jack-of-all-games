4923bb1f58c3f2482ccffcdd3ac73dfcde91f9b7
Thomas McPhee <thomas.mcphee@student.manchester.ac.uk>
2026-03-11 17:16:35 +0000

    changed intermission time

--- static/states/blackjack_states.py
+++ static/states/blackjack_states.py
@@ -11,7 +11,7 @@ class intermission(State):
     def Update(self, dt):
         self.elapsed += dt
 
-        if self.elapsed >= 4:
+        if self.elapsed >= 10:
             self.fsm.SetState("round_start")
 
     def OnExit(self):
